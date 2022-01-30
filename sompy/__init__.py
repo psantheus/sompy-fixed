8df32f7d5b328360595fca0d233a24ad6f34fb0c
Prabhat Sahani <44722647+psantheus@users.noreply.github.com>
2022-01-30 21:25:33 +0530

    update

---
+++ sompy/__init__.py
@@ -0,0 +1,34 @@
+
+from logging.config import dictConfig
+import matplotlib
+
+#matplotlib.use('Agg')  # Use whatever backend is available
+
+dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {
+        "level": "NOTSET",
+        "handlers": ["console"]
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "basic"
+        }
+    },
+    "formatters": {
+        "basic": {
+            "format": '%(message)s'
+        }
+    }
+})
+
+
+#
+# relative imports disabled for temporary development
+from .sompy import SOMFactory
+from .visualization import *
+#from sompy import SOMFactory
+#from visualization import *
